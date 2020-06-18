35747f6e6e27d86cda9be015e42a8998ba860126
AverageLeo <Arieka39@gmail.com>
2020-06-18 14:52:41 +0300

    some editing

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer-text tc light-green br3 pa3 ma2 grow bw2">
+          Made by Arie Levental
+        </div>
+      </div>
+    );
+  }
+}
